e3a4b9fcecc0aaf842f9f8c791836daa608b6f98
Evelin Kolev <evelinkolev@vivaldi.net>
2022-02-23 19:50:29 +0200

    Add Asp.Net MVC Project

---
+++ AutoServiceSystem.Web/Controllers/ClientController.cs
@@ -0,0 +1,23 @@
+﻿using AutoServiceSystem.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoServiceSystem.Web.Controllers
+{
+    [ValidateInput(false)]
+    public class ClientController : Controller
+    {
+        private ClientRepository clientRepository = new ClientRepository();
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            var clients = clientRepository.ReadAll();
+            return View(clients);
+        }
+    }
+}
